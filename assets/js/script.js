5431a9456d66e8b318751fb85a2bc293c03dc4f3
Alyssa Winn <alyssawinn2@hotmail.com>
2021-02-13 09:51:28 -0700

    finish questions; start score function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,35 @@
+//Intro Section
 var startBtn = document.getElementById('start-btn');
 var timerEl = document.getElementById('countdown-timer');
 var introEl = document.getElementById('intro');
+
+//Questions Section
 var questionsEl = document.getElementById('questions');
 var questionContainer = document.createElement("div");
-questionContainer.className = "question-container";
 var timeLeft = 75;
+var questionCounter = 0;
+questionContainer.className = "question-container";
+var questionText = document.createElement("h2");
+questionText.className = "question-text";
+var option1 = document.createElement("button");
+option1.className = "answer-option";
+var option2 = document.createElement("button");
+option2.className = "answer-option";
+var option3 = document.createElement("button");
+option3.className = "answer-option";
+var option4 = document.createElement("button");
+option4.className = "answer-option correct-answer";
+
+//Score Page
+var enterScoreEl = document.getElementById('enter-score');
+var initialsInput = document.createElement("input");
+initialsInput.className = "input-box";
+
 
 var questions = [
-    {q: "Commonly used data types do not include:", a1: "booleans", a2: "strings", a3: "numbers", a4: "alerts"},
-    {q: "The condition in an if/else statement is enclosed with _____", a1: "curly brackets", a2: "square brackets", a3: "quotes", a4: "parenthesis"},
-    {q: "Arrays in JavaScript can be used to store _____", a1: "other arrays", a2: "numbers and strings", a3: "booleans", a4: "all of the above"}
+    {q: "Commonly used data types do not include:", choices: ["booleans", "strings", "numbers", "alerts"], answer: "alerts"},
+    {q: "The condition in an if/else statement is enclosed with _____", choices: [ "curly brackets", "square brackets", "quotes", "parenthesises"], answer: "parenthesises"},
+    {q: "Arrays in JavaScript can be used to store _____", choices: ["other arrays", "numbers and strings", "booleans", "all of the above"], answer: "all of the above"}
 ];
 
 var player = {
@@ -21,45 +41,76 @@ var player = {
     }
 }
 
-function countdown() {
+function clickSubmitHandler(event) {
+    var targetEl = event.target;
+    if (targetEl.matches(".submit-button")) {
+    }
+}
 
+function setScore() {
+    player.score = timeLeft;
+    questionsEl.remove();
+    var scoreContainer = document.createElement("div");
+    var scoreHeader = document.createElement("h1");
+    scoreHeader.textContent = "All done!";
+    var totalScore = document.createElement("h2");
+    totalScore.textContent = "Your final score is " + player.score + " .";
+    var initialsContainer = document.createElement("p");
+    initialsContainer.textContent = "Enter initials: ";
+    var submitButton = document.createElement("button");
+    submitButton.className = "submit-button";
+    submitButton.textContent = "Submit";
+    enterScoreEl.appendChild(scoreContainer);
+    scoreContainer.appendChild(scoreHeader);
+    scoreContainer.appendChild(totalScore);
+    scoreContainer.appendChild(initialsContainer);
+    initialsContainer.appendChild(initialsInput);
+    initialsContainer.appendChild(submitButton);
+};
+
+function countdown() {
     setInterval(function() {
         //start timer
-        if (timeLeft > 0) {
+        if (timeLeft > 0 && player.score == 0) {
             timerEl.textContent = timeLeft;
             timeLeft--;
         }
     }, 1000);
-
     introEl.style.display = 'none';
     createQuestion();
 };
 
 function createQuestion() {
-    //debugger
-    var questionText = document.createElement("h2");
-    questionText.className = "question-text";
-    var option1 = document.createElement("button");
-    option1.className = "answer-option";
-    var option2 = document.createElement("button");
-    option2.className = "answer-option";
-    var option3 = document.createElement("button");
-    option3.className = "answer-option";
-    var option4 = document.createElement("button");
-    option4.className = "answer-option correct-answer";
     questionsEl.appendChild(questionContainer);
     questionContainer.appendChild(questionText);
     questionContainer.appendChild(option1);
     questionContainer.appendChild(option2);
     questionContainer.appendChild(option3);
     questionContainer.appendChild(option4);
+    showQuestion();
+};
 
-    questionText.textContent = questions[0].q;
-    option1.textContent = questions[0].a1;
-    option2.textContent = questions[0].a2;
-    option3.textContent = questions[0].a3;
-    option4.textContent = questions[0].a4;
-
+function showQuestion() {
+    if (questionCounter < questions.length) {
+        questionText.textContent = questions[questionCounter].q;
+        /* var randomArray = [];
+        for (var i=0; i < questions.choices.length; i++) {
+            var randomIndex = [Math.floor(Math.random() * questions.choices.length)];
+            console.log(randomIndex);
+            randomArray.push(questions.choices[randomIndex]);
+          }
+          
+        randomArray.join(",");
+        console.log(randomArray); */
+        option1.textContent = questions[questionCounter].choices[0];
+        option2.textContent = questions[questionCounter].choices[1];
+        option3.textContent = questions[questionCounter].choices[2];
+        option4.textContent = questions[questionCounter].choices[3];
+        questionCounter++;
+    } else {
+        setScore();
+    }
+        
 };
 
 var clickAnswerHandler = function(event) {
@@ -78,8 +129,13 @@ var clickAnswerHandler = function(event) {
         timeLeft = timeLeft - 10;
     }
 
-    setTimeout(createQuestion, 1000);
+    setTimeout(function() {
+        horizontalLine.remove();
+        answerValid.remove();
+        showQuestion();
+    }, 1000);
 };
 
 startBtn.onclick = countdown;
-questionsEl.addEventListener("click", clickAnswerHandler);    
+questionsEl.addEventListener("click", clickAnswerHandler);
+enterScoreEl.addEventListener("click", clickSubmitHandler); 
